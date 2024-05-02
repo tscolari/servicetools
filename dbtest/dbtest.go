800e2dad197efb9e0d71bc19b03b92ec08366de6
Tiago Scolari <git@tscolari.me>
2024-05-02 07:26:34 +0100

    Make `dbtest` more configurable
    Instead of hardcoded default values, this adds the possibility of
    redefining them by using the new exported `Config` object.

--- dbtest/dbtest.go
+++ dbtest/dbtest.go
@@ -18,7 +18,34 @@ import (
 const (
 	defaultUser     = "postgres"
 	defaultPassword = "postgres"
+	defaultDBName   = "postgres"
+	defaultSuffix   = "_test"
 )
+
+// Config contains a set of properties that can be redefined to accomodate
+// different test environments.
+var Config = struct {
+	// Username is the database username used to open the connection.
+	Username string
+
+	// Password is the database password used to open the connection.
+	Password string
+
+	// DBName is the database name to connect to. This is not the same dbname
+	// that the tests will run within.
+	// This is only used to create the initial connection that will then create
+	// the database for the tests (based on the argument given to `DB()`).
+	DBName string
+
+	// DBSuffix is a string that is appended to the database name to
+	// distinguish it from the "non-testing" version.
+	DBSuffix string
+}{
+	Username: defaultUser,
+	Password: defaultPassword,
+	DBName:   defaultDBName,
+	DBSuffix: defaultSuffix,
+}
 
 var initializedDBs map[string]struct{}
 
@@ -33,14 +60,15 @@ func init() {
 // the database.
 // On every call it will truncate all the tables (except the schema one)
 // to ensure that there is no data contamination.
-// It will always append `_test` to the given database name.
+// It will always append `Config.DBSuffix` to the given database name,
+// to have no name appended, set `dbtest.Config.DBSuffix = ""`.
 //
 // Because DB will reset the database on every call, it's not safe for
-// it to be used in parallel tests, unless they are using different
+// this to be used in parallel tests, unless they are using different
 // database names.
 func DB(t *testing.T, migrationsPath, name string) (*gorm.DB, func()) {
 
-	name = name + "_test"
+	name = name + Config.DBSuffix
 
 	var db *gorm.DB
 
@@ -73,7 +101,7 @@ func isDBInitialized(name string) bool {
 }
 
 func initializeDB(t *testing.T, migrationsPath, name string) *gorm.DB {
-	connStr := connectionString(defaultUser, defaultPassword, "postgres")
+	connStr := connectionString(Config.Username, Config.Password, Config.DBName)
 	db, err := gorm.Open(postgres.Open(connStr))
 	require.NoError(t, err, "failed to open DB connection")
 
