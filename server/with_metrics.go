35110b1af2e536a5e3e597db3ce7003ceaa686c9
Tiago Scolari <git@tscolari.me>
2024-05-28 11:50:13 +0100

    Rename healthcheck server to metrics server
    That is probably the best use for that for now.
    Looking into how to bake healthchecks using more elaborate things (e.g.
    validating db connections, etc)

---
+++ server/with_metrics.go
@@ -0,0 +1,75 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"net"
+	"net/http"
+
+	"github.com/heptiolabs/healthcheck"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+// NewWithMetrics returns a WithMetrics object configured with address and metrics.
+func NewWithMetrics(address string, metrics bool) *WithMetrics {
+	return &WithMetrics{
+		address: address,
+		metrics: metrics,
+	}
+}
+
+// WithMetrics implements a simple HTTP server that responds to the `/metrics` endpoint
+// with exposed prometheus metrics.
+type WithMetrics struct {
+	address string
+	metrics bool
+
+	listener net.Listener
+	server   *http.Server
+}
+
+// StartMetrics will start the HTTP metrics server and block
+// until the StopMetrics method is called.
+// This also takes an optional healthHandler, which must implement the healthcheck interface -
+// When provided, that will allow this server to respond to liveness and readiness probes.
+func (h *WithMetrics) StartMetrics(logger *slog.Logger, healthHandler healthcheck.Handler) error {
+
+	lis, err := net.Listen("tcp", h.address)
+	if err != nil {
+		return fmt.Errorf("failed to create listener: %w", err)
+	}
+
+	mux := http.NewServeMux()
+	if healthHandler != nil {
+		mux.Handle("/", healthHandler)
+	}
+
+	if h.metrics {
+		mux.Handle("/metrics", promhttp.Handler())
+	}
+
+	h.server = &http.Server{Handler: mux}
+	h.listener = lis
+
+	logger.Info("starting Metrics Server", "address", h.listener.Addr().String())
+
+	if err := h.server.Serve(h.listener); err != nil {
+		if err != http.ErrServerClosed {
+			return fmt.Errorf("metrics server returned an error: %w", err)
+		}
+	}
+
+	return nil
+}
+
+// StopMetrics will stop the Metrics server and cause Start() to unblock.
+func (h *WithMetrics) StopMetrics(ctx context.Context) error {
+	return h.server.Shutdown(ctx)
+}
+
+// ConfigureMetrics is the hook used by the cmd package to inject the
+// WithMetrics object in the host struct. This must be implemented by the host struct.
+func (h *WithMetrics) ConfigureMetrics(*WithMetrics) {
+	panic("ConfigureMetrics must be implemented")
+}
