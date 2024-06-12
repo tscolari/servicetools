1f1a223f6a8c09a0494ad3f2bedad354957abaae
Tiago Scolari <git@tscolari.me>
2024-06-12 10:32:00 +0100

    Remove gorm dependency from dberrors
    Add more specific error codes to postgres errors base on
    https://www.postgresql.org/docs/current/errcodes-appendix.html

--- database/dberrors/errors.go
+++ database/dberrors/errors.go
@@ -1,13 +1,13 @@
 package dberrors
 
 import (
+	"database/sql"
 	"errors"
 	"strings"
 
 	"github.com/jackc/pgx/v5/pgconn"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	"gorm.io/gorm"
 )
 
 // ToStatusErr converts an error returned from database operations
@@ -28,7 +28,7 @@ func ToStatusErr(err error, msgs ...string) error {
 
 		code := codes.Internal
 
-		if errors.Is(err, gorm.ErrRecordNotFound) {
+		if errors.Is(err, sql.ErrNoRows) {
 			code = codes.NotFound
 		}
 
@@ -37,9 +37,29 @@ func ToStatusErr(err error, msgs ...string) error {
 }
 
 func fromPgConn(err *pgconn.PgError, message string) error {
-	switch err.Code {
-	case "23505":
-		return status.Errorf(codes.AlreadyExists, message, err.Message)
+
+	switch errType := string(err.Code[0] + err.Code[1]); errType {
+	// 08: Connection Exception
+	case "08":
+		return status.Errorf(codes.Unavailable, message, err.Message)
+
+	// 22: Data Exception
+	case "22":
+		return status.Errorf(codes.InvalidArgument, message, err.Message)
+
+	// 23: Integrity Constraint Violation
+	case "23":
+		return status.Errorf(codes.FailedPrecondition, message, err.Message)
+
+	// 28: Invalid Authorization Specification
+	case "28":
+		return status.Errorf(codes.PermissionDenied, message, err.Message)
+
+	// 42: Syntax Error or Access Rule Violation
+	// XX: Internal errors
+	case "42", "XX":
+		return status.Errorf(codes.Internal, message, err.Message)
+
 	}
 
 	return status.Errorf(codes.Unavailable, message, err.Message)
